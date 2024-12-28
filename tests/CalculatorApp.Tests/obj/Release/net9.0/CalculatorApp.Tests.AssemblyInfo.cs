ae8513e774eeb583e52cc464231a1045a43a4d73
KUNAL MAJI <kunalmaji@KUNALs-MacBook-Air.local>
2024-12-28 23:22:31 +0530

    Update pipeline workflow

--- tests/CalculatorApp.Tests/obj/Release/net9.0/CalculatorApp.Tests.AssemblyInfo.cs
+++ tests/CalculatorApp.Tests/obj/Release/net9.0/CalculatorApp.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorApp.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e83551d7d34d72f91cdcef6daef9886282e54021")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c9432da266faf5943b9f51f1bae7dc679391a12")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorApp.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorApp.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
