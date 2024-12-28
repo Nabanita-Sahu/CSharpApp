2a0f1f5341fea2601b84c9bb1de2045d7a719c9d
KUNAL MAJI <kunalmaji@KUNALs-MacBook-Air.local>
2024-12-29 00:28:40 +0530

    final-workflow

--- src/CalculatorApp/obj/Debug/net7.0/CalculatorApp.AssemblyInfo.cs
+++ src/CalculatorApp/obj/Debug/net7.0/CalculatorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cfb0da1a8c68a17d75d00c23f1a3d53edf42f4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cfc2eb1d22148e7e87d8fed3b19e1995c627dec")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
